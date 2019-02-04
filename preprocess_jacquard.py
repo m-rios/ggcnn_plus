efb9b70d22282edb564b45f98d0b8dd6595d183b
Mario <mrm994@gmail.com>
2019-02-04 19:17:18 +0100

    Jacquard preprocessing with no augmentation

--- preprocess_jacquard.py
+++ preprocess_jacquard.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 
 from getpass import getpass
+from ggcnn.dataset_processing import grasp
+from ggcnn.dataset_processing.image import Image, DepthImage
 from scipy import misc
+from skimage import io
 import argparse
+import copy
 import datetime
 import glob
+import matplotlib.pyplot as plt
 import os
 import sys
-import copy
 
 DATASET_PATH = 'data/datasets/raw/jacquard_samples'  # Path to the original dataset files
 OUTPUT_PATH = 'data/datasets/preprocessed'  # Destination of the pre-processed dataset
@@ -16,9 +20,15 @@ OUTPUT_PATH = 'data/datasets/preprocessed'  # Destination of the pre-processed d
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-v', action='store_true', help='visualize only')
+    parser.add_argument('-d', default=DATASET_PATH, help='path to dataset')
+    parser.add_argument('-o', default=DATASET_PATH, help='ouput path of processed dataset')
+    parser.add_argument('--perfect', action='store_true', help='use perfect depth images [default False]')
 
     args = parser.parse_args()
     VISUALIZE_ONLY = args.v
+    DATASET_PATH = args.d
+    OUTPUT_PATH = args.o
+    USE_STEREO = not args.perfect
 
     # Open dataset path
     try:
@@ -55,6 +65,11 @@ if __name__ == '__main__':
 
     description = {}
     description['original_dataset'] = DATASET_PATH.split('/')[-1]
+    description['stereo'] = USE_STEREO
+
+    if VISUALIZE_ONLY:
+        plt.ion()
+        fig, ax = plt.subplots(1,2)
 
     for obj_class in obj_classes:
         obj_class_path = os.path.join(dataset_root, obj_class)
@@ -71,7 +86,26 @@ if __name__ == '__main__':
             print('Processing {}_{}'.format(obj_id, obj_class))
 
             rgb_img_fn = '{}_{}_RGB.png'.format(obj_id, obj_class)
-            stereo_depth_img_fn = '{}_{}_stereo_depth.tiff'.format(obj_id, obj_class)
-            perfect_depth_img_fn = '{}_{}_perfect_depth.tiff'.format(obj_id, obj_class)
+            if USE_STEREO:
+                depth_img_fn = '{}_{}_stereo_depth.tiff'.format(obj_id, obj_class)
+            else:
+                depth_img_fn = '{}_{}_perfect_depth.tiff'.format(obj_id, obj_class)
+            grasp_fn = '{}_{}_grasps.txt'.format(obj_id, obj_class)
 
+            rgb_img_base = Image(io.imread(os.path.join(obj_class_path, rgb_img_fn)))
+            depth_img_base = DepthImage(io.imread(os.path.join(obj_class_path, depth_img_fn)))
+            depth_img_base.inpaint(missing_value=-1)
+            bounding_boxes_base = grasp.BoundingBoxes.load_as_jacquard(os.path.join(obj_class_path,grasp_fn))
 
+            if VISUALIZE_ONLY:
+                ax[0].clear() # remove old bb
+                fig.suptitle(obj_id+'_'+obj_class)
+                rgb_img_base.show(ax[0])
+                bounding_boxes_base.show(ax[0])
+                ax[0].set_title('rgb')
+                depth_img_base.show(ax[1])
+                ax[1].set_title('depth')
+                plt.show()
+                while not plt.waitforbuttonpress():
+                    pass
+                continue
--- visualize_jacquard.py
+++ visualize_jacquard.py
@@ -3,13 +3,16 @@ import cv2
 import os
 import glob
 import matplotlib.pyplot as plt
-from scipy import misc 
+from scipy import misc
+import numpy as np
+from skimage import io
 plt.ion()
 
-DATASET_PATH = 'data/datasets/jacquard_samples'
+DATASET_PATH = 'data/datasets/raw/jacquard_samples'
 
 if __name__ == '__main__':
     root, dir_names, file_names = next(os.walk(DATASET_PATH))
+    fig, ax = plt.subplots(1,3)
     for obj_dir in dir_names:
         obj_path = os.path.join(root, obj_dir)
 
@@ -24,27 +27,36 @@ if __name__ == '__main__':
 
 
             rgb_img = misc.imread(os.path.join(obj_path, rgb_img_fn))
+            rgb_img_2 = io.imread(os.path.join(obj_path, rgb_img_fn))
             stereo_depth_img = misc.imread(os.path.join(obj_path, stereo_depth_img_fn))
+            stereo_depth_img_2 = io.imread(os.path.join(obj_path, stereo_depth_img_fn))
             perfect_depth_img = misc.imread(os.path.join(obj_path, perfect_depth_img_fn))
 
-            # Normalize depth images to [0:255]
-            #stereo_depth_img = (stereo_depth_img - stereo_depth_img.min())/(stereo_depth_img.max() - stereo_depth_img.min())
+            #inpaint stereo
 
-            plt.imshow(rgb_img)
+            stereo_depth_img_2 = cv2.copyMakeBorder(stereo_depth_img_2, 1, 1, 1, 1, cv2.BORDER_DEFAULT)
+            mask = (stereo_depth_img_2 == -1).astype(np.uint8)
+
+            # Scale to keep as float, but has to be in bounds -1:1 to keep opencv happy.
+            scale = np.abs(stereo_depth_img_2).max()
+            stereo_depth_img_2 = stereo_depth_img_2.astype(np.float32) / scale  # Has to be float32, 64 not supported.
+            stereo_depth_img_2 = cv2.inpaint(stereo_depth_img_2, mask, 1, cv2.INPAINT_NS)
+
+            # Back to original size and value range.
+            stereo_depth_img_2 = stereo_depth_img_2[1:-1, 1:-1]
+            stereo_depth_img_2 = stereo_depth_img_2 * scale
+
+            fig.suptitle(obj_id+'_'+obj_dir)
+            ax[0].imshow(rgb_img_2)
+            ax[0].set_title('rgb')
+            ax[1].imshow(perfect_depth_img)
+            ax[1].set_title('perfect depth')
+            ax[2].imshow(stereo_depth_img_2)
+            ax[2].set_title('stereo depth')
+
             plt.show()
             while not plt.waitforbuttonpress():
                 pass
-
-            plt.imshow(perfect_depth_img)
-            plt.show()
-            while not plt.waitforbuttonpress():
-                pass
-
-            plt.imshow(stereo_depth_img)
-            plt.show()
-            while not plt.waitforbuttonpress():
-                pass
-
         # depth_imgs_fn = glob.glob(os.path.join(obj_path, '*.tiff'))
         # rgb_imgs_fn = glob.glob(os.path.join(obj_path, '*.tiff'))
 
