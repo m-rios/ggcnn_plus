6bd2aa277b27899a72caa04ca676505430b50add
Mario <mrm994@gmail.com>
2019-04-01 21:25:18 +0200

    Script to visualize hdf5 dataset

--- visualize_jacquard.py
+++ visualize_jacquard.py
@@ -1,62 +1,23 @@
 #!/usr/bin/env python
-import cv2
-import os
-import glob
-import matplotlib.pyplot as plt
-from scipy import misc
-import numpy as np
-from skimage import io
-plt.ion()
-
-DATASET_PATH = '/Volumes/Data/Jacquard'
+import argparse
+import h5py
+import pylab as plt
+from ggcnn.dataset_processing.grasp import BoundingBoxes, BoundingBox
+parser = argparse.ArgumentParser()
+parser.add_argument('dataset')
+args = parser.parse_args()
 
 if __name__ == '__main__':
-    root, dir_names, file_names = next(os.walk(DATASET_PATH))
-    fig, ax = plt.subplots(1,3)
-    for obj_dir in dir_names:
-        obj_path = os.path.join(root, obj_dir)
+    ds = h5py.File(args.dataset, 'r')
+    depths = ds['test']['depth_inpainted'][:]
+    bbs = ds['test']['bounding_boxes'][:]
 
-        # Isolate object instances
-        objs = set()
-        for fn in os.listdir(obj_path):
-            objs.add(fn.split('_')[0])
-        for obj_id in objs:
-            rgb_img_fn = '{}_{}_RGB.png'.format(obj_id, obj_dir)
-            stereo_depth_img_fn = '{}_{}_stereo_depth.tiff'.format(obj_id, obj_dir)
-            perfect_depth_img_fn = '{}_{}_perfect_depth.tiff'.format(obj_id, obj_dir)
-
-
-            rgb_img = misc.imread(os.path.join(obj_path, rgb_img_fn))
-            rgb_img_2 = io.imread(os.path.join(obj_path, rgb_img_fn))
-            stereo_depth_img = misc.imread(os.path.join(obj_path, stereo_depth_img_fn))
-            stereo_depth_img_2 = io.imread(os.path.join(obj_path, stereo_depth_img_fn))
-            perfect_depth_img = misc.imread(os.path.join(obj_path, perfect_depth_img_fn))
-
-            #inpaint stereo
-
-            stereo_depth_img_2 = cv2.copyMakeBorder(stereo_depth_img_2, 1, 1, 1, 1, cv2.BORDER_DEFAULT)
-            mask = (stereo_depth_img_2 == -1).astype(np.uint8)
-
-            # Scale to keep as float, but has to be in bounds -1:1 to keep opencv happy.
-            scale = np.abs(stereo_depth_img_2).max()
-            stereo_depth_img_2 = stereo_depth_img_2.astype(np.float32) / scale  # Has to be float32, 64 not supported.
-            stereo_depth_img_2 = cv2.inpaint(stereo_depth_img_2, mask, 1, cv2.INPAINT_NS)
-
-            # Back to original size and value range.
-            stereo_depth_img_2 = stereo_depth_img_2[1:-1, 1:-1]
-            stereo_depth_img_2 = stereo_depth_img_2 * scale
-
-            fig.suptitle(obj_id+'_'+obj_dir)
-            ax[0].imshow(rgb_img_2)
-            ax[0].set_title('rgb')
-            ax[1].imshow(perfect_depth_img)
-            ax[1].set_title('perfect depth')
-            ax[2].imshow(stereo_depth_img_2)
-            ax[2].set_title('stereo depth')
-
-            plt.show()
-            while not plt.waitforbuttonpress():
-                pass
-        # depth_imgs_fn = glob.glob(os.path.join(obj_path, '*.tiff'))
-        # rgb_imgs_fn = glob.glob(os.path.join(obj_path, '*.tiff'))
+    for idx in range(depths.shape[0]):
+        fig = plt.figure(figsize=(10, 10))
+        ax = fig.add_subplot(1, 1, 1)
+        gs = BoundingBoxes.load_from_array(bbs[idx])
+        plt.imshow(depths[idx])
+        for g in gs:
+            g.as_grasp.plot(ax)
+        plt.show()
 
