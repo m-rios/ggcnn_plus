5e4a7e87485ceba0a147bf3edc84f0a198e40255
m-rios <mrm994@gmail.com>
2019-05-28 20:22:18 +0200

    Evaluate model node

--- core/network_optimization.py
+++ core/network_optimization.py
@@ -1,13 +1,53 @@
+import h5py
+import core.network as net
+
 from utils.beam_search import BeamSearch
 
 
 class NetworkOptimization(BeamSearch):
 
-    def __init__(self, debug=False):
+    def __init__(self, eval_method, dataset_fn, min_iou=0.25, debug=False):
+        """
+        Optimize GGCNN using Beam Search
+        :param eval_method: string indicating method to use for evaluation. Either 'sim' or 'iou'
+        :param dataset_fn: path to the hdf5 dataset used for the evaluation
+        :param min_iou: minimum iou value to consider a grasp successful (used for iou evaluation only)
+        :param debug: if set to True algorithm will be verbose
+        """
+        assert eval_method == 'iou' or eval_method == 'sim'
+
+        self.eval_method = eval_method
+        self.min_iou = min_iou
+        self.dataset_fn = dataset_fn
+        self._dataset = h5py.File(self.dataset_fn, 'r')
+
+        if self.eval_method == 'iou':
+            self.scenes = self._dataset['test']['img_id'][:]
+            self.depth = self._dataset['test']['depth_inpainted'][:]
+            self.bbs = self._dataset['test']['bounding_boxes'][:]
+        else:
+            pass
+
         super(NetworkOptimization, self).__init__(debug=debug)
 
     def expand(self, node):
         pass
 
+    def _evaluate_iou(self, node):
+        positions, angles, widths = node.predict(self.depth)
+        succeeded, failed = net.calculate_iou_matches(positions, angles, self.bbs,
+                                                      no_grasps=1,
+                                                      grasp_width_out=widths,
+                                                      min_iou=self.min_iou)
+        return float(len(succeeded))/(len(succeeded) + len(failed))
+
+    def _evaluate_sim(self, node):
+        raise NotImplemented('sim evaluation is still not supported')
+
     def evaluate(self, node):
-        pass
+        if self.eval_method == 'iou':
+            return self._evaluate_iou(node)
+        elif self.eval_method == 'sim':
+            return self._evaluate_sim(node)
+        else:  # Failed sanity check
+            raise ValueError('Unrecognised value of eval_method. Expected either sim or iou. Received {}'.format(self.eval_method))
--- test/test_networkOptimization.py
+++ test/test_networkOptimization.py
@@ -1,10 +1,22 @@
 from unittest import TestCase
+import keras
 from core.network_optimization import NetworkOptimization
+from core.network import Network
 
 
 class TestNetworkOptimization(TestCase):
     def test_expand(self):
+        node = Network(model_fn='../ggcnn/data/networks/ggcnn_rss/epoch_29_model.hdf5')
+        op = NetworkOptimization()
+        children, scores, actions = op.expand(node)
         self.fail()
 
     def test_evaluate(self):
-        self.fail()
+        node = Network(model_fn='../ggcnn/data/networks/ggcnn_rss/epoch_29_model.hdf5')
+        op = NetworkOptimization(eval_method='iou', dataset_fn='../data/datasets/preprocessed/jacquard_samples.hdf5')
+        op.min_iou = 0.
+        ev1 = op.evaluate(node)
+        op.min_iou = 1.
+        ev2 = op.evaluate(node)
+        self.assertTrue(ev1 > 0)
+        self.assertTrue(ev2 == 0)
