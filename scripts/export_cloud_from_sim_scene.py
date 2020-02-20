3582880e3303654772f191c730bfbacae88f98bb
Mario <mrm994@gmail.com>
2020-02-20 17:15:37 +0100

    Added script to export simulation scene to npy point cloud

---
+++ scripts/export_cloud_from_sim_scene.py
@@ -0,0 +1,14 @@
+from simulator.simulator import Simulator
+import h5py
+import pptk
+import numpy as np
+
+SCENE_FN = '/Users/mario/Developer/msc-thesis/data/scenes/200220_1700_manually_generated_scenes.hdf5'
+scene = h5py.File(SCENE_FN)['scene'][0]
+sim = Simulator(use_egl=False, gui=False)
+sim.cam.pos = [.6, 0., .3]
+sim.restore(scene, '../data/3d_models/shapenetsem40')
+cloud = sim.cam.point_cloud()
+
+pptk.viewer(cloud)
+np.save('../test/horizontal_bottle.npy', cloud)
